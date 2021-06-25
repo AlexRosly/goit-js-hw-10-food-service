1134c74390bb05db34bd7482340ebda2b3b41ced
AlexRosly <rosly.a777@gmail.com>
2021-06-25 05:12:21 +0300

    Added solve

--- src/index.js
+++ src/index.js
@@ -2,4 +2,45 @@ import descriptionCardTpl from './templates/description-card.hbs';
 import menu from './menu.json'
 import './sass/main.scss';
 
-console.log(descriptionCardTpl(menu));
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuCards = document.querySelector('.js-menu');
+const themeSwitchControl = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+// console.log(themeSwitchControl);
+themeSwitchControl.addEventListener('change', changeSwitchControl);
+
+setLocalStorage();
+
+const markup = descriptionCardTpl(menu);
+menuCards.insertAdjacentHTML('beforeend', markup);
+
+function changeSwitchControl(e) {
+  console.log(e.target.checked);
+  const check = themeSwitchControl.checked;
+  console.log(check);
+  if (check) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+    localStorage.setItem("theme", "dark-theme");
+  } else {
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+    localStorage.setItem("theme", "light-theme");
+
+  }
+}
+
+function setLocalStorage() {
+  const getTheme = localStorage.getItem("theme");
+  console.log(getTheme);
+  if (getTheme === 'dark-theme') {
+    body.classList.add(Theme.DARK);
+    themeSwitchControl.checked = true;
+  } else {
+    body.classList.add(Theme.LIGHT);
+  }
+}
